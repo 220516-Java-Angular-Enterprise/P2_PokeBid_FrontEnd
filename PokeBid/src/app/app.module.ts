f99eb42e43ef753a1e08d13f72d6bf402123b5f3
trknox <love.lala2003@gmail.com>
2022-06-21 16:03:05 -0400

    updated nav-bar

--- PokeBid/src/app/app.module.ts
+++ PokeBid/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { NavBarComponent } from './common/header/nav-bar/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
