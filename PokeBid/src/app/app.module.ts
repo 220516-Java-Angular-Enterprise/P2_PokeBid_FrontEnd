230dd99c36ceb0b94dc53a5bcf2a37ae3d463e21
trknox <love.lala2003@gmail.com>
2022-06-21 16:08:16 -0400

    installed material

--- PokeBid/src/app/app.module.ts
+++ PokeBid/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { NavBarComponent } from './common/header/nav-bar/nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavBarComponent } from './common/header/nav-bar/nav-bar/nav-bar.compone
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
