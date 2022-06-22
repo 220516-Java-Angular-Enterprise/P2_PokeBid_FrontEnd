86cbcc639b635623ca55a8194fc9bf608317b433
HaibunisCool <97471066+HaibunisCool@users.noreply.github.com>
2022-06-21 22:20:32 -0500

    Added getAllUsers and getUserById to userService

---
+++ PokeBid/src/app/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
+import {firstValueFrom} from 'rxjs';
+import { User } from '../models/users';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http:HttpClient) { }
+
+  private userURL = "http://localhost:8080/pokebid/users"
+  
+  getAllUsers(): Promise<User[]>{
+    return firstValueFrom(this.http.get<User[]>(this.userURL));
+  }
+
+  getUserById(id: string): Promise<User>{
+    return firstValueFrom(this.http.get<User>(this.userURL + "/" + id))
+  }
+}
