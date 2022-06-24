4ebf8caf701cbbf842e8f3291b49f24681973188
HaibunisCool <97471066+HaibunisCool@users.noreply.github.com>
2022-06-23 22:23:52 -0500

    Refined header successfully to match theme.

--- PokeBid/src/app/common/header/nav-bar/nav-bar/pinned/pinned.component.ts
+++ PokeBid/src/app/common/header/nav-bar/nav-bar/pinned/pinned.component.ts
@@ -1,3 +1,6 @@
+import { ICard } from './../../../../../models/pokemon/pokemon';
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { Pinned } from '../../../../../models/pinned';
 import { Component, OnInit } from '@angular/core';
 import { CardListing } from 'src/app/models/cardListing';
 import { Condition } from 'src/app/models/condition';
@@ -14,14 +17,21 @@ import { PinnedService } from 'src/app/services/pinned.service';
 })
 export class PinnedComponent implements OnInit {
 
-  constructor(private pinnedService: PinnedService, private http:HttpClient) { }
+  constructor(private pinnedService: PinnedService, private http:HttpClient, private pokemon: PokemonService) { }
 
   
-  fullPinCards: any[] = [];
+  fullPinCards: Pinned[] = [];
+
 
   ngOnInit(): void {
 
-    this.pinnedService.getPinned().subscribe( (cardlisting) => this.fullPinCards = cardlisting );
+    this.pinnedService.getPinned().subscribe( (cardlisting) => {this.fullPinCards = cardlisting
+    this.fullPinCards.forEach(pinned => {
+      this.pokemon.getCardById(pinned.cardListing.card_id).subscribe((json) => {
+        pinned.cardListing.card_name = json.data[0].name
+      })
+    })
+    });
   }
 
 }
--- PokeBid/src/app/models/pinned.ts
+++ PokeBid/src/app/models/pinned.ts
@@ -1,4 +1,9 @@
+import { CardListing } from './cardListing';
+import { User } from './users';
 
 export interface Pinned{
-  
+  id: string;
+  user: User;
+  cardListing: CardListing;
+
 }
