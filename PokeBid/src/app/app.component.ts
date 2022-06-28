910ca5cf532a770e299acf1ebda1803a7ce0087e
hislittlecuzin <hislittlecuzingames@gmail.com>
2022-06-28 14:42:45 -0400

    Something

--- PokeBid/src/app/app.component.ts
+++ PokeBid/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { AuthService, AuthState, User } from '@auth0/auth0-angular';
+import {HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -9,8 +12,25 @@ import { environment } from 'src/environments/environment';
 export class AppComponent {
   title = 'PokeBid';
 
-  constructor(){
+  isLoggedIn = false;
+  user: any = {}
+
+  ngOnInit(){
+    this.auth.isAuthenticated$.subscribe(b => {
+      this.isLoggedIn = b;
+      });
+      this.auth.user$.subscribe(u => {
+        this.user = u;
+        console.log("HERE IS THE OTHER OTHER OTHER LOG!!!!");
+      console.log(this.user);
+      });
+      console.log("HERE IS THE LOG!!!!");
+      console.log(this.user);
+  }
   
+
+  constructor(public auth: AuthService){
+    
   }
 
 
